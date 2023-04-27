27745b504723d118223871969dc811d0d3000c94
bvanstratum <brian.vanstratum@gmail.com>
2023-04-27 17:24:19 -0400

    got a linear controller working

---
+++ params2array.m
@@ -0,0 +1,11 @@
+function ParamArray = decodeParams(param)
+I1 = param.I1; 
+I2 = param.I2 ;
+m1 = param.m1 ;
+m2 = param.m2 ;
+mb = param.mb ;
+L1 = param.L1 ;
+L2 = param.L2 ;
+g  = param.g  ;
+ParamArray = [I1,I2,m1,m2,mb,L1,L2,g];
+end
